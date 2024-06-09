ba5a44af1ff149a92746785984c4c481d6ef1756
akkas_dev <codersadhin@gmail.com>
2024-06-09 22:25:04 +0800

    change again

--- O_N_square.cpp
+++ O_N_square.cpp
@@ -4,20 +4,20 @@ int main()
 {
     int n;
     cin>>n;
-    for(int i=1;i<=n;i++) // O(N*N)
-    {
-        for(int j=1;j<=n;j++)
-        {
-            cout<<"hello"<<endl;
-        }
-    }
-
-    // for(int i=0;i<=n-1;i++) // O(N*N)
+    // for(int i=1;i<=n;i++) // O(N*N)
     // {
-    //     for(int j=i+1;j<n;j++)
+    //     for(int j=1;j<=n;j++)
     //     {
-
+    //         cout<<"hello"<<endl;
     //     }
     // }
+
+    for(int i=0;i<=n-1;i++) // O(N*N)
+    {
+        for(int j=i+1;j<n;j++)
+        {
+
+        }
+    }
     return 0;
 }
